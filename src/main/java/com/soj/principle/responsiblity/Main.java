61fc9e29ad8f39985d5a30048122122000a202c0
test@gmail.com <test@gmail.com>
2022-02-14 13:58:48 +0000

    customer commit

--- src/main/java/com/soj/principle/responsiblity/Main.java
+++ src/main/java/com/soj/principle/responsiblity/Main.java
@@ -1,7 +1,47 @@
 package com.soj.principle.responsiblity;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
-    public static void main(String[] args){
+    static final Logger logger= LogManager.getLogger(Main.class.getName());
+    public static void main(String[] args) {
+        Customer customer = new Customer();
+        customer.setName("Johjn");
+        customer.setAddress("Pune");
+
+        Order order = new Order();
+        order.setItemName("Pizza4");
+        order.setQuantity(1);
+        order.setCustomer(customer);
+        order.setItemPrice(100);
+        String prepareOrder=order.prepareOrder();
+        logger.debug(prepareOrder);
+        BillCalculation billCalculation = new BillCalculation(order);
+        String calculateBill=billCalculation.calculateBill();
+        logger.debug(calculateBill);
+
+        DeliveryApp deliveryApp = new DeliveryApp(order);
+        String delivery=deliveryApp.delivery();
+        logger.debug(delivery);
+
+        Customer customer1 = new Customer();
+        customer1.setName("John");
+        customer1.setAddress("Pune3");
+        Order order1 = new Order();
+        order1.setItemName("Pizza");
+        order1.setQuantity(2);
+        order1.setCustomer(customer1);
+        order1.setItemPrice(200);
+        String prepareOrder1=order1.prepareOrder();
+        logger.debug(prepareOrder1);
+        BillCalculation billCalculation1 = new BillCalculation(order1);
+        String calculateBill1=billCalculation1.calculateBill();
+        logger.debug(calculateBill1);
+
+        DeliveryApp deliveryApp1 = new DeliveryApp(order1);
+        String delivery1=deliveryApp1.delivery();
+        logger.debug(delivery1);
 
     }
 }
---
+++ src/main/java/com/soj/principle/responsiblity/Order.java
@@ -0,0 +1,70 @@
+package com.soj.principle.responsiblity;
+
+
+import java.util.Random;
+
+public class Order {
+    Random random = new Random();
+    private Customer customer;
+    private String orderId;
+    private String itemName;
+    private int quantity;
+    private int itemPrice;
+    private int totalBillAmt;
+
+    public int getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(int itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+
+        this.orderId = orderId + "_" + random.nextInt(500);
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+        setOrderId(itemName);
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getTotalBillAmt() {
+        return totalBillAmt;
+    }
+
+    public void setTotalBillAmt(int totalBillAmt) {
+        this.totalBillAmt = totalBillAmt;
+    }
+
+    public String prepareOrder() {
+        return "Preparing order for customer - " +
+                this.getCustomer().getName() + "" +
+                "who has ordered " + this.getItemName();
+    }
+}
